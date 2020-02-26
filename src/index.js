d59557c4d256f131131ce3c5a131b880f06a3570
lucasoz <luemunozmo@unal.edu.co>
2020-02-26 08:22:13 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import SiderContainer from "./side/Sider";
+import "./index.css";
+
+ReactDOM.render(<SiderContainer />, document.getElementById("root"));
