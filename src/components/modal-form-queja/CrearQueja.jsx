397f23687d2e8abfe493c72432dd52b3f6280de3
Lucas Home <luemunozmo@unal.edu.co>
2020-02-26 23:19:43 -0500

    some fix on send queja to firebase, loader working

--- src/components/modal-form-queja/CrearQueja.jsx
+++ src/components/modal-form-queja/CrearQueja.jsx
@@ -11,15 +11,14 @@ class CrearQueja extends React.Component {
     this.props.form.resetFields();
     this.props.setModalVisible(false);
   }
-  
 
-  handleSubmit = async (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.setState({ loading: true });
-    const { apto, titulo, descripcion, timePiker: { _d } } = this.props.form.getFieldsValue();
 
-    await this.props.form.validateFields(async (err, values) => {
+    this.props.form.validateFields(async (err, values) => {
       if (!err) {
+        const { apto, titulo, descripcion, timePiker: { _d } } = this.props.form.getFieldsValue();
         await firestore.collection('quejas').add({
           apto,
           titulo,
@@ -28,8 +27,8 @@ class CrearQueja extends React.Component {
         });
         this.closeModal();
       }
+      this.setState({ loading: false });
     });
-    this.setState({ loading: false });
   };
 
   render() {
@@ -55,56 +54,56 @@ class CrearQueja extends React.Component {
         ]}
       >
         <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item label="Apartamento involucrado">
-          {getFieldDecorator('apto', {
-            rules: [{ required: true, message: 'Por favor selecciona un Apartamento!' }],
-          })(
-            <Select
-              prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Apartamento"
-              showSearch
-              optionFilterProp="children"
-              filterOption={(input, option) =>
-                option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-            >
-              <Select.Option value="1001">1001</Select.Option>
-              <Select.Option value="1002">1002</Select.Option>
-              <Select.Option value="1003">1003</Select.Option>
-              <Select.Option value="1004">1004</Select.Option>
-              <Select.Option value="2001">2001</Select.Option>
-              <Select.Option value="2002">2002</Select.Option>
-              <Select.Option value="2003">2003</Select.Option>
-              <Select.Option value="2004">2004</Select.Option>
-            </Select>,
-          )}
-        </Form.Item>
-        <Form.Item label="Título">
-          {getFieldDecorator('titulo', {
-            rules: [{ required: true, message: 'Ingresa un título!' }],
-          })(
-            <Input
-              prefix={<Icon type="issues-close" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Título"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item label="Descripción del hecho">
-          {getFieldDecorator('descripcion', {
-            rules: [{ required: true, message: 'Añade una descripción de lo sucedido!' }],
-          })(
-            <Input
-              prefix={<Icon type="info-circle" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Descripción"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item label="Fecha">
-          {getFieldDecorator('timePiker', {
-            rules: [{ required: true, message: 'Fecha en la que sucedió el hecho' }],
-          })(<DatePicker placeholder="Fecha" style={{width: "100%"}}/>)}
-        </Form.Item>
-      </Form>
+          <Form.Item label="Apartamento involucrado">
+            {getFieldDecorator('apto', {
+              rules: [{ required: true, message: 'Por favor selecciona un Apartamento!' }],
+            })(
+              <Select
+                prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Apartamento"
+                showSearch
+                optionFilterProp="children"
+                filterOption={(input, option) =>
+                  option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                }
+              >
+                <Select.Option value="1001">1001</Select.Option>
+                <Select.Option value="1002">1002</Select.Option>
+                <Select.Option value="1003">1003</Select.Option>
+                <Select.Option value="1004">1004</Select.Option>
+                <Select.Option value="2001">2001</Select.Option>
+                <Select.Option value="2002">2002</Select.Option>
+                <Select.Option value="2003">2003</Select.Option>
+                <Select.Option value="2004">2004</Select.Option>
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item label="Título">
+            {getFieldDecorator('titulo', {
+              rules: [{ required: true, message: 'Ingresa un título!' }],
+            })(
+              <Input
+                prefix={<Icon type="issues-close" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Título"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label="Descripción del hecho">
+            {getFieldDecorator('descripcion', {
+              rules: [{ required: true, message: 'Añade una descripción de lo sucedido!' }],
+            })(
+              <Input
+                prefix={<Icon type="info-circle" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Descripción"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label="Fecha">
+            {getFieldDecorator('timePiker', {
+              rules: [{ required: true, message: 'Fecha en la que sucedió el hecho' }],
+            })(<DatePicker placeholder="Fecha" style={{width: "100%"}}/>)}
+          </Form.Item>
+        </Form>
       </Modal>
     );
   };
