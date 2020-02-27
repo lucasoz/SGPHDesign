8d8e877830f98a40253f4187be9c587c44915a09
Lucas Muñoz <lemunos@geosat.com.co>
2020-02-27 15:40:41 -0500

    eslint added

--- src/utils/notifications.jsx
+++ src/utils/notifications.jsx
@@ -3,7 +3,7 @@ import { notification, Icon } from 'antd';
 
 export const notiSuccess = (description) => (
   notification.error({
-    closeIcon: <Icon type="close" style={{color: "white"}} />,
+    closeIcon: <Icon type="close" style={{color: "white" }} />,
     className: 'successNoti',
     description,
     icon: <Icon type="check-circle" style={{color: "white"}} />,
