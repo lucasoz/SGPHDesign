e5893b630084cf5e432bd0f890a915acf8e4ba36
Lucas Home <luemunozmo@unal.edu.co>
2020-02-26 21:31:53 -0500

    enrutamiento añadido

--- src/components/side/Sider.jsx
+++ src/components/side/Sider.jsx
@@ -1,11 +1,12 @@
 // SEBAS este componente es https://ant.design/components/layout/ pero usa el sider
 import React from "react";
+import { Switch, Route, Redirectm, Link } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb, Icon, Modal, Button } from "antd";
+import { withRouter } from 'react-router-dom';
 import ListaNoticias from "../noticias/ListaNoticias";
 import CrearQueja from "../modal-form-queja/CrearQueja";
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Header, Content, Footer} = Layout;
 
 class SiderContainer extends React.Component {
   state = {
@@ -25,6 +26,7 @@ class SiderContainer extends React.Component {
   }
 
   render() {
+    const { history } = this.props;
     return (
         <Layout>
           <Header style={{
@@ -40,13 +42,13 @@ class SiderContainer extends React.Component {
               defaultSelectedKeys={['1']}
               style={{ lineHeight: '64px' }}
             >
-              <Menu.Item key="HOME">
+              <Menu.Item key="HOME" onClick={()=>history.push("/")}>
                 <Icon type="home" /> SGPH
               </Menu.Item>
               <Menu.Item key="queja">
                 <Icon type="plus-circle" /> Queja
               </Menu.Item>
-              <Menu.Item key="listar-quejas">
+              <Menu.Item key="listar-quejas" onClick={()=>history.push("/quejas")}>
                 <Icon type="warning" /> Listar Quejas
               </Menu.Item>
               <Menu.Item key="4">nav 4</Menu.Item>
@@ -54,7 +56,10 @@ class SiderContainer extends React.Component {
           </Header>
           <Content style={{ padding: '10px 10px', marginTop: 64 }}>
             <CrearQueja modalQueja={this.state.modalQueja} setModalVisible={this.setModalVisible} />
-            <ListaNoticias />
+            <Switch>
+              <Route exact path="/" component={ListaNoticias} />
+              <Route exact path="/quejas" component={"aasdawdwad"} />
+            </Switch>
           </Content>
           <Footer style={{ textAlign: "center" }}>
             Ant Design ©2018 Created by Ant UED
@@ -64,4 +69,4 @@ class SiderContainer extends React.Component {
   }
 }
 
-export default SiderContainer;
+export default withRouter(SiderContainer);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 import "antd/dist/antd.css";
 import SiderContainer from "./components/side/Sider";
 import "./index.css";
 
-ReactDOM.render(<SiderContainer />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <SiderContainer />
+  </BrowserRouter>, document.getElementById("root"));
