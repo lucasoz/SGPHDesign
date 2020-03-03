9a4c095fb6e3a2bc4b9ecc144bcfc79cca5280f3
Lucas Muñoz <lemunos@geosat.com.co>
2020-03-02 19:56:51 -0500

    adelanto

---
+++ src/components/card-usuario/CardUsuario.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Card,
+  Spin,
+  Typography,
+  Divider,
+} from 'antd';
+import {
+  LoadingOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import moment from 'moment';
+import { firestore } from '../../firebase/firebase.utils';
+
+const { Meta } = Card;
+const { Text } = Typography;
+
+class CardUsuario extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      apartamento: null,
+    };
+  }
+
+  componentDidMount() {
+    const { usuario } = this.props;
+    this.getAsyncData(usuario);
+  }
+
+  getAsyncData = async ({ apto }) => {
+    firestore.collection('propiedades');
+    
+    //this.setState({ apartamento });
+  }
+
+
+  render() {
+    const {
+      usuario: {
+        nombre,
+        apellido,
+        cedula,
+        fechaNacimiento,
+      },
+    } = this.props;
+
+    const { apartamento } = this.state;
+
+    return (
+      <Card>
+        <Meta
+          avatar={(
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <UserOutlined style={{ fontSize: '3em', marginBottom: '5px', color: '#1890ff' }} />
+              {
+                !apartamento
+                  ? <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+                  : <Text strong>{apartamento}</Text>
+              }
+            </div>
+          )}
+          title={`${nombre} ${apellido}`}
+          description={`Cédula: ${cedula}`}
+        />
+        <Divider dashed />
+        <Text strong>{moment.unix(fechaNacimiento.seconds).locale('es').format('MMMM DD YYYY')}</Text>
+        <Divider dashed />
+        historial de habitabilidad
+      </Card>
+    );
+  }
+}
+
+export default CardUsuario;
---
+++ src/components/usuarios/Usuarios.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { List } from 'antd';
+import _ from 'lodash';
+import CardUsuario from '../card-usuario/CardUsuario';
+import { firestore } from '../../firebase/firebase.utils';
+import { notiError } from '../../utils/notifications';
+
+class Usuarios extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usuarios: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getCollection();
+  }
+
+  getCollection = async () => {
+    firestore.collection('usuarios')
+      .onSnapshot(
+        (snapshot) => snapshot.forEach((usuario) => {
+          this.setState((state) => ({
+            usuarios: { ...state.usuarios, [usuario.id]: { ...usuario.data(), id: usuario.id } },
+          }));
+        }),
+        (error) => notiError(error),
+      );
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const {
+      usuarios,
+      loading,
+    } = this.state;
+
+    return (
+      <div>
+        <List
+          loading={loading}
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 4,
+            xl: 4,
+            xxl: 5,
+          }}
+          dataSource={_.toArray(usuarios)}
+          renderItem={(item) => (
+            <List.Item>
+              <CardUsuario usuario={item} />
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default Usuarios;
